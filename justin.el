83969bb4aaaa1fecc209cbb3abc7b3950be4d3a6
Justin Richter <jrichter@jetfive.com>
2011-11-25 15:33:02 -0600

    initial add of my personal dir for emacs prelude

---
+++ justin.el
@@ -0,0 +1,19 @@
+;; Set initial layout
+(setq default-frame-alist
+      '((top . 2) (left . 22) (width . 129) (height . 50)))
+
+;; Keybinding for commenting region
+(global-set-key (kbd "C-;") 'comment-or-uncomment-region)
+
+;; rhtml mode https://github.com/eschulte/rhtml.git
+(add-to-list 'load-path "~/.emacs.d/personal/rhtml")
+(require 'rhtml-mode)
+
+;; Set text to 12pt
+(set-face-attribute 'default nil :height 120)
+
+;; Clean up buffers before save
+(add-hook 'before-save-hook 'whitespace-cleanup)
+
+;; (setq whitespace-newline 'whitespace-newline)
+(setq whitespace-display-mappings '((newline-mark 10 [182 10])))
